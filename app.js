81724629a0ab1894f4474f35ef3d552d58f7db3a
Vladislav <vladislavfrontdev@gmail.com>
2022-06-08 18:48:11 +0300

    add variables

---
+++ app.js
@@ -0,0 +1,6 @@
+const input = document.querySelector(".inputFields")
+const check = document.querySelectorAll("#done")
+const plus = document.querySelector(".plusButton")
+
+//console.log(check[0].checked)
+//input.value
