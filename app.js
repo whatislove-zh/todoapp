b80516b86250dcb40592e88c15ac7dcfd2ed3815
Vladislav <vladislavfrontdev@gmail.com>
2022-06-09 18:36:01 +0300

    adding some function
    *function for adding tasks
    *function for crossing the line
    *function for deleting tasks

--- app.js
+++ app.js
@@ -1,6 +1,64 @@
-const input = document.querySelector(".inputFields")
-const check = document.querySelectorAll("#done")
-const plus = document.querySelector(".plusButton")
+const input = document.querySelector(".inputFields");
+let check = document.querySelectorAll("#done");
+let label = document.querySelectorAll(".label");
+const plus = document.querySelector(".plusButton");
+const todoList = document.querySelector(".todoList");
+let deleteBtn = document.querySelectorAll(".deleteButton");
 
-//console.log(check[0].checked)
-//input.value
+let counter = 2;
+
+//function for adding tasks
+const addTask = () => {
+  const block = document.createElement("div");
+  block.innerHTML = `<input type="checkbox" id="done" name="task1" />
+    <label class = "label">${input.value}</label>
+    <i class="fa-solid fa-eraser deleteButton"></i>`;
+  todoList.append(block);
+  check = document.querySelectorAll("#done");
+  label = document.querySelectorAll(".label");
+  deleteBtn = document.querySelectorAll(".deleteButton");
+  input.value = "";
+  deleteEvent();
+  checked();
+};
+
+//function for crossing the line
+const crossLine = () => {
+  for (i = 0; i < check.length; i++) {
+    if (check[i].checked) {
+      //console.log(label)
+      label[i].innerHTML = `<s>${label[i].innerText}</s>`;
+    } else {
+      //console.log(label)
+      label[i].innerHTML = `${label[i].innerText}`;
+    }
+  }
+};
+
+//function for deleting tasks
+function deleteTask() {
+  this.parentNode.remove();
+}
+
+//function for adding event listener1
+const checked = () => {
+  for (i = 0; i < check.length; i++) {
+    check[i].addEventListener("click", crossLine);
+  }
+  crossLine();
+};
+//function for adding event listener2
+const deleteEvent = () => {
+  for (i = 0; i < deleteBtn.length; i++) {
+    deleteBtn[i].addEventListener("click", deleteTask);
+  }
+};
+
+window.addEventListener("load", checked);
+window.addEventListener("load", deleteEvent);
+plus.addEventListener("click", addTask);
+input.addEventListener("keydown", function (btn) {
+  if (btn.key === "Enter") {
+    addTask();
+  }
+});
